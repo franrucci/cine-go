592e22ac1b61a6f306009055d8f3936b3187eab2
franrucci <franrucci01@gmail.com>
2026-04-01 09:55:21 -0300

    agregué DataAnnotations

--- cine-go-mvc/Models/Pelicula.cs
+++ cine-go-mvc/Models/Pelicula.cs
@@ -1,17 +1,28 @@
-﻿namespace cine_go_mvc.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace cine_go_mvc.Models
 {
     public class Pelicula
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
         public string Titulo { get; set; }
+        [DataType(DataType.Date)]
         public DateTime FechaLanzamiento { get; set; }
+        [Range(1, 500)]
         public int MinutosDuracion { get; set; }
+        [Required]
+        [StringLength(0)]
         public string Sinopsis { get; set; }
+        [Required]
         public string PosterUrlPortada { get; set; }
         public int GeneroId { get; set; }
         public Genero? Genero { get; set; }
         public int PlataformaId { get; set; }
         public Plataforma? Plataforma { get; set; }
+        [NotMapped]
         public int PromedioRating { get; set; }
         public List<Review>? ListaReviews { get; set; }
         public List<Favorito>? UsuariosFavorito { get; set; }
--- cine-go-mvc/Models/Plataforma.cs
+++ cine-go-mvc/Models/Plataforma.cs
@@ -1,10 +1,16 @@
-﻿namespace cine_go_mvc.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace cine_go_mvc.Models
 {
     public class Plataforma
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
         public string Nombre { get; set; }
+        [Url]
         public string Url { get; set; }
+        [Url]
         public string LogoUrl { get; set; }
         public List<Pelicula>? PeliculasPlataforma { get; set; }
     }
