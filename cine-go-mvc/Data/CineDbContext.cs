f2219dd5c68f8c84d7e3c568a9e42f3f267a543a
franrucci <franrucci01@gmail.com>
2026-04-01 09:57:37 -0300

    configuración entity e identity

---
+++ cine-go-mvc/Data/CineDbContext.cs
@@ -0,0 +1,19 @@
+﻿using cine_go_mvc.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace cine_go_mvc.Data
+{
+    public class CineDbContext : IdentityDbContext<Usuario>
+    {
+        public CineDbContext(DbContextOptions<CineDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Pelicula> Peliculas { get; set; }
+        public DbSet<Genero> Generos { get; set; }
+        public DbSet<Plataforma> Plataformas { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Favorito> Favoritos { get; set; }
+    }
+}
