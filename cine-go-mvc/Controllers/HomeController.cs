c4f2505206ac5a0202f206975386b4c3cafd085e
franrucci <franrucci01@gmail.com>
2026-04-06 16:41:25 -0300

    diseño de vista Home

--- cine-go-mvc/Controllers/HomeController.cs
+++ cine-go-mvc/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
+using cine_go_mvc.Data;
 using cine_go_mvc.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace cine_go_mvc.Controllers
@@ -7,15 +9,18 @@ namespace cine_go_mvc.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly CineDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, CineDbContext cineDbContext)
         {
             _logger = logger;
+            _context = cineDbContext;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var peliculas = await _context.Peliculas.ToListAsync();
+            return View(peliculas);
         }
 
         public IActionResult Privacy()
