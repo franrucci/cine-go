96022f6070e4a282a63dcd72e8f366be3397d542
franrucci <franrucci01@gmail.com>
2026-04-17 15:37:35 -0300

    implementa creación de reseñas

---
+++ cine-go-mvc/Controllers/ReviewController.cs
@@ -0,0 +1,113 @@
+﻿using cine_go_mvc.Data;
+using cine_go_mvc.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.CodeDom;
+
+namespace cine_go_mvc.Controllers
+{
+    public class ReviewController : Controller
+    {
+        private readonly UserManager<Usuario> _userManager;
+        private readonly CineDbContext _context;
+        public ReviewController(UserManager<Usuario>userManager, CineDbContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+        // GET: ReviewController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ReviewController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ReviewController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ReviewController/Create
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ReviewCreateViewModel review)
+        {
+            try
+            {
+                review.UsuarioId = _userManager.GetUserId(User);
+
+                if (ModelState.IsValid)
+                {
+                    var nuevaReview = new Review
+                    {
+                        PeliculaId = review.PeliculaId,
+                        UsuarioId = review.UsuarioId,
+                        Rating = review.Rating,
+                        Comentario = review.Comentario,
+                        FechaReview = DateTime.Now
+                    };
+                    _context.Reviews.Add(nuevaReview);
+                    _context.SaveChanges();
+                    return RedirectToAction("Details", "Home", new { id = review.PeliculaId });
+                }
+
+                return View(review);
+            }
+            catch
+            {
+                return View(review);
+            }
+        }
+
+        // GET: ReviewController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ReviewController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ReviewController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ReviewController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
